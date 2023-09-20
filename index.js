6540e6878e7b6c1c75cd2e93ea9b3ca2d75abb8c
liulka_s <asl.warmhaus@gmail.com>
2023-09-20 20:21:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,34 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      const allContacts = await contacts.listContacts();
-      console.table(allContacts);
-      break;
+  try {
+    switch (action) {
+      case 'list':
+        const allContacts = await contacts.listContacts();
+        console.table(allContacts);
+        break;
 
-    case 'get':
-      const contactById = await contacts.getContactById(id);
-      console.log(contactById);
-      break;
+      case 'get':
+        const contactById = await contacts.getContactById(id);
+        console.log(contactById);
+        break;
 
-    case 'add':
-      const newContact = await contacts.addContact(name, email, phone);
-      console.log(newContact);
-      break;
+      case 'add':
+        const newContact = await contacts.addContact(name, email, phone);
+        console.log(newContact);
+        break;
 
-    case 'remove':
-      const removeContact = await contacts.removeContact(id);
-      console.log(removeContact);
-      break;
+      case 'remove':
+        const removeContact = await contacts.removeContact(id);
+        console.log(removeContact);
+        break;
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  } catch {
+    console.log(error.message);
+    throw error;
   }
 }
 
